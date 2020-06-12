549ee0dd13fcafcb3125a38a6fafcdf566e89b53
Matthew Barnes <taking1n1@gmail.com>
2020-06-12 21:20:38 +0100

    Hid divider and bottom of project card if all filters are unchecked

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -19,30 +19,32 @@
     <v-card-text>{{ project.summary }}</v-card-text>
 
     <!-- Chips -->
-    <v-divider class="mx-4"></v-divider>
-    <v-card-text>
+    <template v-if="shouldShowChips">
+      <v-divider class="mx-4"></v-divider>
+      <v-card-text>
 
-      <!-- Languages -->
-      <v-chip v-for="language in project.languages" :key="language" v-if="showLanguages" color="red" class="project-chip mr-1 mb-2">
-        {{ languageStore.find(language).name }}
-      </v-chip>
+        <!-- Languages -->
+        <v-chip v-for="language in project.languages" :key="language" v-if="showLanguages" color="red" class="project-chip mr-1 mb-2">
+          {{ languageStore.find(language).name }}
+        </v-chip>
 
-      <!-- Frameworks -->
-      <v-chip v-for="framework in project.frameworks" :key="framework" v-if="showFrameworks" color="blue" class="project-chip mr-1 mb-2">
-        {{ frameworkStore.find(framework).name }}
-      </v-chip>
+        <!-- Frameworks -->
+        <v-chip v-for="framework in project.frameworks" :key="framework" v-if="showFrameworks" color="blue" class="project-chip mr-1 mb-2">
+          {{ frameworkStore.find(framework).name }}
+        </v-chip>
 
-      <!-- Technologies -->
-      <v-chip v-for="technology in project.technologies" :key="technology" v-if="showTechnologies" color="green" class="project-chip mr-1 mb-2">
-        {{ technologyStore.find(technology).name }}
-      </v-chip>
+        <!-- Technologies -->
+        <v-chip v-for="technology in project.technologies" :key="technology" v-if="showTechnologies" color="green" class="project-chip mr-1 mb-2">
+          {{ technologyStore.find(technology).name }}
+        </v-chip>
 
-      <!-- Tags -->
-      <v-chip v-for="tag in project.tags" :key="tag" v-if="showTags" color="purple" class="project-chip mr-1 mb-2">
-        {{ tagStore.find(tag).name }}
-      </v-chip>
+        <!-- Tags -->
+        <v-chip v-for="tag in project.tags" :key="tag" v-if="showTags" color="purple" class="project-chip mr-1 mb-2">
+          {{ tagStore.find(tag).name }}
+        </v-chip>
 
-    </v-card-text>
+      </v-card-text>
+    </template>
 
   </v-card>
 
@@ -74,6 +76,13 @@
 
     /** If true, tag chips are shown */
     @Prop({default: true, type: Boolean}) showTags!: boolean;
+
+    /**
+     * If true, we should show chips. If false, we should hide chips.
+     */
+    private get shouldShowChips(): boolean {
+      return this.showLanguages || this.showFrameworks || this.showTechnologies || this.showTags;
+    }
   }
 </script>
 
