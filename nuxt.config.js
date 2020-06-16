4e678998ca515167c459bdb012c2a498f2327a18
Matthew Barnes <taking1n1@gmail.com>
2020-06-16 16:26:26 +0100

    Fiddled around with vuetify colours
    ... to get it to work with heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-const colors = require('vuetify/es5/util/colors').default;
 require("dotenv").config();
 
 module.exports = {
@@ -68,21 +67,7 @@ module.exports = {
   ** https://github.com/nuxt-community/vuetify-module
   */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    optionsPath: './vuetify.options.js'
   },
   /*
   ** Build configuration
