b78ae848531cdbf57d788be82a0b60d2fd7644c6
Matthew Barnes <mb2g17@gmail.com>
2021-06-06 12:54:56 +0100

    Added git dropdown in header

--- components/Header.vue
+++ components/Header.vue
@@ -20,7 +20,21 @@
       <v-tab nuxt to="/">Home</v-tab>
       <v-tab nuxt to="/projects">Projects</v-tab>
       <v-tab nuxt to="/aboutme">About Me</v-tab>
-      <v-tab href="https://github.com/mb2g17">My GitHub</v-tab>
+
+      <v-menu auto eager offset-y>
+        <template v-slot:activator="{on, attrs}">
+          <v-tab v-bind="attrs" v-on="on">Git</v-tab>
+        </template>
+        <v-list>
+          <v-list-item href="https://git.matt-barnes.co.uk" target="_blank">
+            <v-list-item-title>Gitea (self-hosted)</v-list-item-title>
+          </v-list-item>
+          <v-list-item href="https://github.com/mb2g17" target="_blank">
+            <v-list-item-title>GitHub</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-tab nuxt to="/changelog">Changelog</v-tab>
     </v-tabs>
 
